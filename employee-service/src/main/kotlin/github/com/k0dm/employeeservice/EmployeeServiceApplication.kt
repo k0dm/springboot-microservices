396bb36391828a3cace62f72e7b00fb00058407e
k0dm <fighttournament2@gmail.com>
2023-11-10 09:41:44 +0200

    circuit breaker and retry implemented

--- employee-service/src/main/kotlin/github/com/k0dm/employeeservice/EmployeeServiceApplication.kt
+++ employee-service/src/main/kotlin/github/com/k0dm/employeeservice/EmployeeServiceApplication.kt
@@ -2,12 +2,14 @@ package github.com.k0dm.employeeservice
 
 import feign.Capability
 import feign.micrometer.MicrometerCapability
+import io.github.resilience4j.circuitbreaker.CircuitBreaker
 import io.micrometer.core.instrument.MeterRegistry
 import org.modelmapper.ModelMapper
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.context.annotation.Bean
+import org.springframework.web.reactive.function.client.WebClient
 
 @EnableFeignClients
 @SpringBootApplication
@@ -17,6 +19,9 @@ class EmployeeServiceApplication {
 	fun capability(registry: MeterRegistry?): Capability {
 		return MicrometerCapability(registry)
 	}
+
+	@Bean
+	fun webClient() = WebClient.builder().build()
 
 	@Bean
 	fun modelMapper() = ModelMapper()
--- employee-service/src/main/kotlin/github/com/k0dm/employeeservice/service/EmployeeService.kt
+++ employee-service/src/main/kotlin/github/com/k0dm/employeeservice/service/EmployeeService.kt
@@ -1,19 +1,26 @@
 package github.com.k0dm.employeeservice.service
 
 import github.com.k0dm.employeeservice.dto.APIResponseDto
+import github.com.k0dm.employeeservice.dto.DepartmentDto
 import github.com.k0dm.employeeservice.dto.EmployeeDto
 import github.com.k0dm.employeeservice.entity.Employee
 import github.com.k0dm.employeeservice.exception.EmailAlreadyExistsException
 import github.com.k0dm.employeeservice.exception.ResourceNotFoundException
 import github.com.k0dm.employeeservice.repository.EmployeeRepository
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker
+import io.github.resilience4j.retry.annotation.Retry
 import org.modelmapper.ModelMapper
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
+import org.springframework.web.reactive.function.client.WebClient
 
 interface EmployeeService {
 
     fun createEmployee(employeeDto: EmployeeDto): EmployeeDto
 
     fun getEmployeeById(id: Long): APIResponseDto
+
+    fun getDefaultDepartment(id: Long, e: Exception): APIResponseDto
 
     fun getEmployeeByEmail(email: String): EmployeeDto
 
@@ -23,8 +30,11 @@ interface EmployeeService {
     class Base(
         private val repository: EmployeeRepository,
         private val modelMapper: ModelMapper,
-        private val apiClient: APIClient
+        //private val apiClient: APIClient
+        private val webClient: WebClient
     ) : EmployeeService {
+
+        private val LOGGER = LoggerFactory.getLogger(EmployeeService::class.java)
 
         override fun createEmployee(employeeDto: EmployeeDto): EmployeeDto {
             val email = employeeDto.email
@@ -37,14 +47,40 @@ interface EmployeeService {
             )!!
         }
 
-
+       // @CircuitBreaker(name = "\${spring.application.name}", fallbackMethod = "getDefaultDepartment")
+        @Retry(name = "\${spring.application.name}", fallbackMethod = "getDefaultDepartment")
         override fun getEmployeeById(id: Long): APIResponseDto {
+
+            LOGGER.info("inside getEmployeeById() method")
 
             val employee = repository.findById(id).orElseThrow {
                 throw ResourceNotFoundException("Employee", "id", id)
             }
             val employeeDto = modelMapper.map(employee, EmployeeDto::class.java)
-            val departmentDto = apiClient.getDepartmentByCode(employee.departmentCode)
+            // val departmentDto = apiClient.getDepartmentByCode(employee.departmentCode)
+            val departmentDto = webClient.get()
+                .uri("http://localhost:8080/api/departments/byCode/" + employee.departmentCode)
+                .retrieve()
+                .bodyToMono(DepartmentDto::class.java)
+                .block()!!
+
+            return APIResponseDto(employeeDto, departmentDto)
+        }
+
+        override fun getDefaultDepartment(id: Long, e: Exception): APIResponseDto {
+            LOGGER.info("inside getDefaultDepartment() method")
+
+            val employee = repository.findById(id).orElseThrow {
+                throw ResourceNotFoundException("Employee", "id", id)
+            }
+            val employeeDto = modelMapper.map(employee, EmployeeDto::class.java)
+
+            val departmentDto = DepartmentDto(
+                null,
+                "R&D Department",
+                "Research and Development Department",
+                "RD001"
+            )
 
             return APIResponseDto(employeeDto, departmentDto)
         }
