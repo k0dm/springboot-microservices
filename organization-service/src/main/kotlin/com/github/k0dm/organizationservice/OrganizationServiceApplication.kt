d70209fc0c419cdc845cd847c341673d177e8545
k0dm <fighttournament2@gmail.com>
2023-11-10 17:42:32 +0200

    full implementation Organization-Service (eureka client, cloud config, spring bus and metrics)

--- organization-service/src/main/kotlin/com/github/k0dm/organizationservice/OrganizationServiceApplication.kt
+++ organization-service/src/main/kotlin/com/github/k0dm/organizationservice/OrganizationServiceApplication.kt
@@ -1,5 +1,8 @@
 package com.github.k0dm.organizationservice
 
+import feign.Capability
+import feign.micrometer.MicrometerCapability
+import io.micrometer.core.instrument.MeterRegistry
 import org.modelmapper.ModelMapper
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
@@ -7,6 +10,11 @@ import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
 class OrganizationServiceApplication{
+
+	@Bean
+	fun capability(registry: MeterRegistry?): Capability {
+		return MicrometerCapability(registry)
+	}
 
 	@Bean
 	fun modelMapper() = ModelMapper()
